7018e45f7c4b7da44d5a615b0df309bbc12f7f1b
Dounan Shi <dounan@gmail.com>
2013-03-20 17:24:25 -0700

    use new tokenizer in test.py

--- test.py
+++ test.py
@@ -1,13 +1,19 @@
 from mysql_tweets import MysqlTweets
 from ngram_kneser_ney import NgramKneserNey
 from sentence_generator import SentenceGenerator
+from happyfuntokenizing import Tokenizer
+import twokenize
 
-N = 4
+N = 3
 
-t = MysqlTweets()
+# tokenize = Tokenizer(preserve_case=False).tokenize
+tokenize = twokenize.tokenize
+
+
+t = MysqlTweets(tokenize)
 kn = NgramKneserNey(N)
 print "Getting sentences"
-sentences = t.get_more_sentences(100000)
+sentences = t.get_more_sentences(1000)
 print "Training model"
 kn.train(sentences)
 generator = SentenceGenerator(kn)
